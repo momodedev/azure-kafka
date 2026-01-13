48a4e5b9d7957a20030619c9e3348bcfff0a25fa
Mtian <v-chengzhiz@microsoft.com>
2026-01-13 14:08:01 +0800

    fix ports

--- setup_control_node_terraform/private_vmss_init.sh
+++ setup_control_node_terraform/private_vmss_init.sh
@@ -6,6 +6,9 @@ wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/sh
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform jq -y
 
+# Explicitly disable UFW to ensure ports 3000/9090 are reachable relative to Azure NSG
+sudo ufw disable
+
 sudo apt update
 sudo apt install python3-venv -y
 
